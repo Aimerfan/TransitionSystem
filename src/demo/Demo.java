858f42a919c000d5f9715500b77950c9a342eda8
Moty <zzser15963@gmail.com>
2019-01-09 02:51:27 +0800

    sdf

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,30 +1,31 @@
 package demo;
 
-import java.util.ArrayList;
-
-import data.Book;
 import data.BorrowerData;
 import data.LenderData;
 import model.Borrower;
 import model.Goods;
 import model.Lender;
 
+import java.util.ArrayList;
+
+import data.Book;
+
 public class Demo {
 
-  public static void main(String[] args) {
-    Borrower borrower = new Borrower(new BorrowerData("John", "L123456789"));
-    Borrower borrower1 = new Borrower(new BorrowerData("John", "L123456789"));
+	public static void main(String[] args) {
+		Borrower borrower = new Borrower(new BorrowerData("John", "L123456789"));
+		Borrower borrower1 = new Borrower(new BorrowerData("John", "L123456789"));
+		
+		System.out.println(borrower.equals(borrower1));
+		
+		Book [] books = {new Book("可怕的家锪","恐懼"),new Book("開心的家園","開勳")};
+		ArrayList<Goods> arrayBooks = new ArrayList<Goods>();
+		for(Book b: books) arrayBooks.add(b);
+		
+		Lender lender = new Lender(new LenderData("John", "L123456789"),arrayBooks );
+		System.out.println(lender.toString());
+		
 
-    System.out.println(borrower.equals(borrower1));
-
-    Book[] books = { new Book("可怕的家锪", "恐懼"), new Book("開心的家園", "開勳") };
-    ArrayList<Goods> arrayBooks = new ArrayList<Goods>();
-    for (Book b : books)
-      arrayBooks.add(b);
-
-    Lender lender = new Lender(new LenderData("John", "L123456789"), arrayBooks);
-    System.out.println(lender.toString());
-
-  }
+	}
 
 }
