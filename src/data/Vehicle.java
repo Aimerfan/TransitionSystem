c7e24cb14c60db67dd5aec74d4f12d4559f281f9
Moty <zzser15963@gmail.com>
2019-01-08 19:36:31 +0800

    修改Vehicle

--- src/data/Vehicle.java
+++ src/data/Vehicle.java
@@ -1,5 +1,32 @@
 package data;
 
-public class Vehicle {
+import model.Goods;
 
+public class Vehicle extends Goods{
+	private String name;
+	private String type;
+	
+	public Vehicle(String name, String type) {
+		super();
+		setName(name);
+		setType(type);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String toString() {
+		return "Name: " + name + " Type: " + type;
+		
+	}
 }
