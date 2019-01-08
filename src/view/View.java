0ffe4aa7b21cada35c59e19886085960ca8675f3
harry151 <harry151@mail.fcu.edu.tw>
2019-01-09 01:50:02 +0800

    微側

--- src/view/View.java
+++ src/view/View.java
@@ -44,35 +44,22 @@ public class View {
 
     panel.setLayout(null);
 
-    // 创建 JLabel
     JLabel userLabel = new JLabel("使用者:");
-    /*
-     * 这个方法定义了组件的位置。 setBounds(x, y, width, height) x 和 y 指定左上角的新位置，由 width 和 height
-     * 指定新的大小。
-     */
     userLabel.setBounds(10, 20, 80, 25);
     panel.add(userLabel);
 
-    /*
-     * 创建文本域用于用户输入
-     */
     JTextField userText = new JTextField(20);
     userText.setBounds(100, 20, 165, 25);
     panel.add(userText);
 
-    // 输入密码的文本域
     JLabel passwordLabel = new JLabel("密碼:");
     passwordLabel.setBounds(10, 50, 80, 25);
     panel.add(passwordLabel);
 
-    /*
-     * 这个类似用于输入的文本域 但是输入的信息会以点号代替，用于包含密码的安全性
-     */
     JPasswordField passwordText = new JPasswordField(20);
     passwordText.setBounds(100, 50, 165, 25);
     panel.add(passwordText);
 
-    // 创建登录按钮
     JButton loginButton = new JButton("登入");
     loginButton.setBounds(10, 80, 80, 25);
     panel.add(loginButton);
@@ -126,25 +113,19 @@ public class View {
   }
 
   public boolean confirmationView(String Name) {
-    // 初始化一个jframe
+
     confirm = new JFrame("Comfirm");
 
-    // 初始化一个文字区域
     JTextArea textarea = new JTextArea(Name);
 
-    // 初始化一个jlable
     JLabel emptyLable = new JLabel("產品資訊寫這邊");
 
-    // 初始化一个panel
     JPanel panel = new JPanel();
 
-    // 初始化一个容器
     Container container = confirm.getContentPane();
 
-    // 初始化一个按钮
     JButton startAutoGen = new JButton("確認資料");
 
-    // 设置布局
     startAutoGen.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         isConfirm = true;
@@ -152,21 +133,16 @@ public class View {
       }
     });
 
-    // 把按钮添加到pannel
     panel.add(textarea);
     panel.add(emptyLable);
     panel.add(startAutoGen);
 
-    // 设置关闭方式
     confirm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    // 把panel添加到容器
     container.add(panel);
 
-    // 设置大小
     confirm.setSize(350, 200);
 
-    // 设置可见性
     confirm.setVisible(true);
 
     while (true) {
